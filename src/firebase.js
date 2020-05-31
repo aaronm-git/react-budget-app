1133941d1a40f5bd9bacae0a3163c807721d68a9
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2020-05-31 18:12:29 -0400

    firebase auth added

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+import * as firebase from "firebase/app";
+import 'firebase/analytics';
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGER_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+// Initialize Firebase
+class Firebase {
+  constructor() {
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    this.auth = firebase.auth();
+  }
+
+  login = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
+  logout = () => this.auth.signOut();
+  register = async (name, email, password) => {
+    await this.auth.createUserWithEmailAndPassword(email, password);
+    return this.auth.currentUser.updateProfile({ displayName: name });
+  };
+}
+
+export default new Firebase();
